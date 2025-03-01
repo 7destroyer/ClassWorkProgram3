feca9a11aa07a634ea58e2e230942c85ebce84d6
Anton <antoha357357@gmail.com>
2025-03-01 17:18:20 +0300

    ..

--- ClassWorkProgram3/ClassWorkProgram3.cpp
+++ ClassWorkProgram3/ClassWorkProgram3.cpp
@@ -10,7 +10,7 @@ void multiplyMatrix(int** matrix, int rows, int cols, int scalar);
 
 int main()
 {
-    setlocale(LC_ALL, "Ru");
+    setlocale(LC_ALL, "ru");
 
     int rows = 2;
     int cols = 2;
